47b754c5cd99fc2d54a630a5f53bc2b56cc77506
farhanakhan05 <farhana19feb2004@gmail.com>
2026-01-30 18:25:17 +0530

    Initial commit: Symptom to Department Chatbot using TF-IDF and Naive Bayes

--- Chatbot.py
+++ Chatbot.py
@@ -1,5 +1,14 @@
-while true:
-  user_input=input ("Enter Symptoms(on type exit)")
-  if user_input.lower()="exit":
-  break
-  print("Suggested Department:",predict department(user input))
+from Model import predict_department
+
+print("symptoms to department chatbot")
+print("type 'exit' to quit")
+
+while True:
+    symptoms = input("Enter symptoms: ")
+
+    if symptoms.lower() == "exit":
+        print("Goodbye")
+        break
+
+    department = predict_department(symptoms)
+    print("Recommended Department:", department)
--- Model.py
+++ Model.py
@@ -1,3 +1,25 @@
 import pandas as pd
-from sklearn.feature_extraction.text import TfidVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
+
+#load dataset
+data = pd.read_csv("data/symptom_department.csv")
+
+X = data['Symptoms_text']
+y = data['Department']
+
+#vectorize symptoms
+vectorizer = TfidfVectorizer()
+X = vectorizer.fit_transform(X)
+
+#train model
+model = MultinomialNB()
+model.fit(X, y)
+
+#predict department
+def predict_department(Symptoms):
+  Symptoms_tfidf = vectorizer.transform([Symptoms])
+  department = model.predict(Symptoms_tfidf)
+  return department[0]
+
+print(predict_department("chest pain"))
